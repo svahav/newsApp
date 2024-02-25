12bda053c63ef2e415463722ec3b04b372c4ef73
Shira <shirash@matrix.co.il>
2024-02-25 10:59:51 +0200

    improvements to NewsService & NewsController

--- force-app/main/default/lwc/newsComponent/newsComponent.js
+++ force-app/main/default/lwc/newsComponent/newsComponent.js
@@ -1,3 +1,15 @@
 import { LightningElement } from 'lwc';
-
-export default class NewsComponent extends LightningElement {}
+import getNewsData from '@salesforce/apex/NewsController.getNewsData' ;
+export default class NewsComponent extends LightningElement {
+    
+    connectedCallback(){
+        this.fetchNews();
+    }
+    fetchNews(){
+        getNewsData().then(response=>{
+            console.log(response);
+        }).catch(error=>{
+            cpnsole.error(error);
+        })
+    }
+}
