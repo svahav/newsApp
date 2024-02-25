62512de556b909ca1974b8461599a53ea982b671
Shira <shirash@matrix.co.il>
2024-02-25 14:31:19 +0200

    LWC News Component build

--- force-app/main/default/lwc/newsComponent/newsComponent.js
+++ force-app/main/default/lwc/newsComponent/newsComponent.js
@@ -1,15 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track } from 'lwc';
 import getNewsDataFromService from '@salesforce/apex/NewsController.getNewsDataFromService' ;
 export default class NewsComponent extends LightningElement {
-    
+    @track result= [];
     connectedCallback(){
         this.fetchNews();
     }
     fetchNews(){
         getNewsDataFromService().then(response=>{
             console.log(response);
+            this.formatNewsData(response.articles)             
         }).catch(error=>{
-            cpnsole.error(error);
+            console.error(error);
         })
+    }
+    formatNewsData(res){
+        this.result = res.map((item, index)=>{
+            let id = `new_${index+1}`;
+            let name = item.source.name;
+            return {...item, id:id, name:name}
+        })
+        
     }
 }
